3c60b6e0209b511b1e368698b83c781b8f59f0e5
Eber Peña <eberg.pena@gmail.com>
2020-05-08 02:06:39 -0500

    Se agrego querystring para buscar el pais de countryist por MX,CO, etc...

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 var http = require("http");
+var url = require("url");
+var querystring = require("querystring");
 //Mandar llamar importaciones
 var log = require("./modules/my-log");
 var consts = require("./utils/consts");
@@ -7,30 +9,38 @@ var firabese = require("../libs/firebase");
 var { countries } = require("countries-list");
 
 var server = http.createServer(function (request, response) {
-  if (request.url === "/") {
+  var parsed = url.parse(request.url);
+  console.log("parsed", parsed);
+
+  var pathname = parsed.pathname;
+
+  var query = querystring.parse(parsed.query);
+  console.log("query", query);
+
+  if (pathname === "/") {
     response.writeHead(200, { Content_type: "text/html" });
     response.write("<html><body><p>HOME</p></body></html>");
     response.end();
-  } else if (request.url === "/exit") {
+  } else if (pathname === "/exit") {
     response.writeHead(200, { Content_type: "text/html" });
     response.write("<html><body><p>BYE</p></body></html>");
     response.end();
     //Cambiar conuntry xINFO
-  } else if (request.url === "/info") {
+  } else if (pathname === "/info") {
     //Variable URL
-    var result = log.info(request.url);
+    var result = log.info(pathname);
     response.writeHead(200, { Content_type: "text/html" });
     response.write(result);
     response.end();
     //Consultar country list
-  } else if (request.url === "/country") {
+  } else if (pathname === "/country") {
     //Variable URL
-    var result = log.info(request.url);
+    var result = log.info(pathname);
     response.writeHead(200, { Content_type: "application/json" });
-    response.write(JSON.stringify(countries.MX));
+    response.write(JSON.stringify(countries[query.code]));
     response.end();
-  } else if (request.url === "/error") {
-    var result = log.error(request.url);
+  } else if (pathname === "/error") {
+    var result = log.error(pathname);
     response.writeHead(200, { Content_type: "text/html" });
     response.write(result);
     response.end();
